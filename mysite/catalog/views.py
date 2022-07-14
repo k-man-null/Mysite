4fd12b8a0473689eaf468e4054c12193e133bdde
Konchellah <williamoriginal2019@gmail.com>
2022-07-14 17:53:13 +0300

    Added view function,basetemplate and index template

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
+def index(request):
+    """View function for the homepage of the site"""
 
+    #Generate counts of some of the main objects
+
+    num_books = Book.objects.all().count()
+
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.filter(name__icontains="Fiction").count()
+
+    num_books_filtered = Book.objects.filter(title__icontains="a").count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_filtered': num_books_filtered
+    }
+
+
+    return render(request, 'index.html', context=context)
