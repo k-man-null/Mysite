d611ad532157df888ef367423bc207dc8d535dec
Konchellah <williamoriginal2019@gmail.com>
2022-07-22 23:57:34 +0300

    Added Generic Editing Forms

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -13,6 +13,11 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from catalog.forms import RenewBookForm
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
 # Create your views here.
 
 def index(request):
@@ -121,3 +126,21 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView,PermissionRequiredMixin):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+    permission_required = 'catalog.can_mark_returned'
+
+class AuthorUpdate(UpdateView,PermissionRequiredMixin):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+    permission_required = 'catalog.can_mark_returned'
+class AuthorDelete(DeleteView,PermissionRequiredMixin):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+    permission_required = 'catalog.can_mark_returned'
